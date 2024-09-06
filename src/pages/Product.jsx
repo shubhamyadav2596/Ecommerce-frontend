77028a5f170d7caedfb624b1b07923b0e6e60001
progammerBoy <sy8692935@gmail.com>
2024-09-06 14:18:27 +0530

    minor changes

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -23,7 +23,7 @@ const Product = () => {
 
   useEffect(() => {
     fetchProductData();
-  }, [productId]);
+  }, [productId, products]);
 
   return productsData ? (
     <div className="border-t-2 pt-10 transition-opacity ease-in duration-500 opacity-100">
